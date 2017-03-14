db3ecc85aa96baccfb803bcb35ea64bd13887907
Chris Zempel <zempel@efeqdev.com>
2017-03-14 17:46:49 -0500

    Almost working rendering via express. Having troubles with ReactRouter.match

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Route, IndexRoute} from 'react-router';
+import {Route} from 'react-router';
 import Layout from './components/Layout';
 import AboutPage from './components/AboutPage';
+import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
   <Route path="/" component={Layout}>
-    <IndexRoute component={AboutPage}/>
+    <Route path="/about" component={AboutPage}/>
+    <Route path="*" component={NotFoundPage}/>
   </Route>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,6 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
       mangle: true,
-      sourcemap: false,
       beautify: false,
       dead_code: true
     }),
